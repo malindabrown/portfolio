93807ef11ea72ef5a8c97d69258af8a3facff786
malindabrown <malinda@malindabrown.com>
2018-01-21 13:33:19 -0500

    'gallery need hero removed on small screen'

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,6 +1,27 @@
-body {
-   font-family: "abril-fatface", "Ostrich Sans Rounded Medium", "Ostrich Sans", "Ostrich Sans Black", "Ostrich Sans Bold", "Ostrich Sans Medium", "Ostrich Sans Light", "Ostrich Sans Heavy"; 
-   margin: 0;
-   padding: 0;
-   color: pink;
+const[current, imgs, opacity] = [
+   document.querySelector('#current'), document.querySelectorAll('.imgs img'),
+   0.8 
+];
+
+// Set First img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imageClick));
+
+function imageClick(e){
+   // Remove Opacity
+   imgs.forEach(img => (img.style.opacity = 1));
+
+   // Change current image to src of clicked image
+   current.src = e.target.src;
+
+   // Add Fade-in Class
+   current.classList.add('fade-in');
+
+   // Remove Fade-In Class after .5 seconds
+   setTimeout(() => current.classList.remove('fade-in'), 500);
+
+   // Change the opacity to opacity var
+   e.target.style.opacity = opacity;
+   
 }
