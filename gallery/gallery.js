50487eb4e1f7b4ca928458ec448f5653c3088b2f
malindabrown <malinda@malindabrown.com>
2018-01-21 11:15:00 -0500

    'set up gallery'

---
+++ gallery/gallery.js
@@ -0,0 +1,6 @@
+body {
+   font-family: "abril-fatface", "Ostrich Sans Rounded Medium", "Ostrich Sans", "Ostrich Sans Black", "Ostrich Sans Bold", "Ostrich Sans Medium", "Ostrich Sans Light", "Ostrich Sans Heavy"; 
+   margin: 0;
+   padding: 0;
+   color: pink;
+}
